3267d5bbea6e56e244d59927e8a496fb5478cf24
JohanEmsgard <emsgard.johan@elev.ga.ntig.se>
2022-08-24 13:15:19 +0200

    hfbsj

---
+++ Sonicgame/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sonic");
+Console.WriteLine("These are your stats");
+
+Newclass s1 = new  ();
+
+s1.name = "Sonic";
+
+s1.walk -= 10;
+s1.Superspeed += 10;
+
+Console.WriteLine(s1.walk);
+Console.WriteLine(s1.Superspeed);
+
+Console.ReadLine();
